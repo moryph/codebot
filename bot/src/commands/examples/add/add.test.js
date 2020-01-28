cf0e0917ab92f92d9f6c3514f5657818aa911a69
moryph <moryph@protonmail.com>
2020-01-28 15:24:06 -0500

    Update add test with better test cases.

--- bot/src/commands/examples/add/add.test.js
+++ bot/src/commands/examples/add/add.test.js
@@ -6,8 +6,8 @@ function formatOutput (num1, num2, sum) {
 
 test('Greet Test', async () => {
   expect(await add.handler(1, 1)).toEqual(formatOutput(1, 1, 2))
-  expect(await add.handler(10, 10)).toEqual(formatOutput(10, 10, 20))
+  expect(await add.handler(10, -10)).toEqual(formatOutput(10, -10, 0))
   expect(await add.handler(313, 291)).toEqual(formatOutput(313, 291, 604))
-  expect(await add.handler(15, 34)).toEqual(formatOutput(15, 34, 49))
+  expect(await add.handler(1.5, 3.4)).toEqual(formatOutput(1.5, 3.4, 4.9))
   expect(await add.handler(90, 90)).toEqual(formatOutput(90, 90, 180))
 })
