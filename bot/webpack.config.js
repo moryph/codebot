fb73d98cf1ea47b28fef4f43938e7c7f112ba9d8
moryph <moryph@protonmail.com>
2020-01-28 14:12:30 -0500

    Enable nodemon webpack plugin

--- bot/webpack.config.js
+++ bot/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const NodemonPlugin = require('nodemon-webpack-plugin');
 
 module.exports = {
 
@@ -10,18 +10,19 @@ module.exports = {
     filename: 'bundle.js'
   },
   target: 'node',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
+          loader: 'babel-loader'
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new NodemonPlugin()
+  ]
 };
